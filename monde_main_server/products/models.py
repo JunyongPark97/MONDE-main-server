7012767c9e5b1778cfae51209533b8856eb95da7
Ubuntu <ubuntu@ip-172-31-35-174.ap-northeast-2.compute.internal>
2019-08-27 08:23:45 +0000

    [FIXED] product models

--- monde_main_server/products/models.py
+++ monde_main_server/products/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Bag(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+
