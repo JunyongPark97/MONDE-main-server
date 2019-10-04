c45d58b4bc5647701a953327d1371a7939f49fe6
JunyongPark97 <pjyong68@naver.com>
2019-10-04 18:38:18 +0900

    [FIX] method delete to post

--- monde_main_server/user_activities/views.py
+++ monde_main_server/user_activities/views.py
@@ -36,7 +36,7 @@ class RecentViewLogViewSet(viewsets.GenericViewSet,
 
         return paginated_response
 
-    @action(detail=False, methods=['delete'])
+    @action(detail=False, methods=['post'])
     def delete(self, request, *args, **kwargs):
         """
         여러개를 한번에 삭제하기 위해 @action으로 사용.
