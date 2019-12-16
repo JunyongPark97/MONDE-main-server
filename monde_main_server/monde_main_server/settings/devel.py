74236a7d391b7f6af8ce0c44dde1a887c1a1cf4a
JunyongPark97 <pjyong68@naver.com>
2019-12-16 13:13:50 +0900

    [ADD] static url for devel

--- monde_main_server/monde_main_server/settings/devel.py
+++ monde_main_server/monde_main_server/settings/devel.py
@@ -14,3 +14,6 @@ MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware',] + MIDDLEWARE
 
 INTERNAL_IPS = ('127.0.0.1', '15.164.101.147')
 
+AWS_ACCESS_KEY_ID = load_credential('PROD_AWS_ACCESS_KEY_ID', "")
+AWS_SECRET_ACCESS_KEY = load_credential('PROD_AWS_SECRET_ACCESS_KEY', "")
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
